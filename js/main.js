7597ad97795932c1a65d4066e36376bbf7ff2313
Jerry Lott <jerry.l.lott@gmail.com>
2022-01-07 20:12:50 -0600

    Added autonav function to js file

--- js/main.js
+++ js/main.js
@@ -8,3 +8,13 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
+/* Auto open SideNav on bigger screes and close on smaller */
+function autoNav(){
+  window.addEventListener("resize", function() {
+    if (window.matchMedia("(min-width: 1200px)").matches) {
+        openNav();
+    } else {
+        closeNav();
+     }
+    })
+}
