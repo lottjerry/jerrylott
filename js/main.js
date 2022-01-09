78f16395cb4045fe02fd35ff05595afd03763a94
Jerry Lott <jerry.l.lott@gmail.com>
2022-01-08 18:09:03 -0600

    Added open and close button functionality, got rid of close button on sideNav

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("open").style.display="none";
+  document.getElementById("close").style.display="inline"
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("close").style.display="none";
+  document.getElementById("open").style.display="inline"
 }
 
 /* Auto open SideNav on bigger screes and close on smaller */
 function autoNav(){
+  document.getElementById("open").style.display="inline"
   window.addEventListener("resize", function() {
     if (window.matchMedia("(min-width: 1200px)").matches) {
         openNav();
