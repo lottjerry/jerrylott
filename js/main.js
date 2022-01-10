ef6df90f07ce992a1636cc5904eafd56c7eb1aa8
Jerry Lott <jerry.l.lott@gmail.com>
2022-01-09 19:24:01 -0600

    Added z-index and fixed issue with closebtn showing up on refresh on smaller devices

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("open").style.display="none";
-  document.getElementById("close").style.display="inline"
+  document.getElementById("openbtn").style.display="none";
+  document.getElementById("closebtn").style.display="inline";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("close").style.display="none";
-  document.getElementById("open").style.display="inline"
+  document.getElementById("closebtn").style.display="none";
+  document.getElementById("openbtn").style.display="inline";
 }
 
 /* Auto open SideNav on bigger screes and close on smaller */
 function autoNav(){
-  document.getElementById("open").style.display="inline"
+  document.getElementById("openbtn").style.display="inline";
+  document.getElementById("closebtn").style.display="none";
   window.addEventListener("resize", function() {
     if (window.matchMedia("(min-width: 1200px)").matches) {
         openNav();
