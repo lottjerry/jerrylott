fb05dc951d3c3a2e35d7298787f3c0d8a088f911
Jerry Lott <jerry.l.lott@gmail.com>
2022-04-23 23:10:02 -0500

    Fix to form not going to thankyou page after gatcha

--- js/main.js
+++ js/main.js
@@ -50,3 +50,7 @@ function toTop() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+ //email
+ const email = 'jerry@jerrylott.com';
+ const encodedEmail = encodeURIComponent(email);
+ const url = `https://formsubmit.co/${encodedEmail}`;
