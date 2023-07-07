f8f1c63ec48e98417fde75624a2badcca932f76c
Jerry Lott <jj@Jerrys-Mac-mini.local>
2023-07-07 11:24:33 -0500

    Made Navbar a component and fixed the spacing for the default layout

---
+++ components/Navbar.vue
@@ -0,0 +1,73 @@
+<template>
+	<v-navigation-drawer width="175" theme="dark">
+		<div class="d-flex justify-end pa-1">
+			<v-btn @click="toggleTheme" variant="text" size="avatar">
+				<v-icon v-if="mode === 'myTheme'">ph:moon</v-icon>
+				<v-icon v-else>ph:sun</v-icon>
+			</v-btn>
+		</div>
+		<div class="d-flex justify-center">
+			<v-avatar size="avatar">
+				<img src="../assets/img/home-img.jpg" alt="Jerry Lott" height="150" />
+			</v-avatar>
+		</div>
+		<div class="text-center text-h6 font-weight-bold pb-3">Jerry Lott</div>
+
+		<v-divider></v-divider>
+		<v-list density="compact" class="d-flex justify-center">
+			<v-btn
+				variant="text"
+				size="avatar"
+				class="mr-5"
+				href="https://www.linkedin.com/in/jerrylott/"
+				target="_blank"
+				rel="noopener"
+			>
+				<v-icon>ph:linkedin-logo-duotone</v-icon>
+			</v-btn>
+			<v-btn
+				variant="text"
+				size="avatar"
+				href="https://github.com/lottjerry"
+				target="_blank"
+				rel="noopener"
+			>
+				<v-icon>ph:github-logo-duotone</v-icon>
+			</v-btn>
+		</v-list>
+		<v-divider></v-divider>
+		<v-list density="compact" nav class="mt-5">
+			<NuxtLink to="/">
+				<v-list-item
+					prepend-icon="ph:house-thin"
+					title="Home"
+					value="home"
+					rounded="xl"
+					class="mb-5"
+				></v-list-item
+			></NuxtLink>
+			<NuxtLink to="/about"
+				><v-list-item
+					prepend-icon="ph:user-thin"
+					title="About"
+					value="about"
+					rounded="xl"
+					class="mb-5"
+				></v-list-item
+			></NuxtLink>
+		</v-list>
+	</v-navigation-drawer>
+</template>
+
+<script setup>
+import { useTheme } from 'vuetify';
+const theme = useTheme();
+let mode = ref();
+
+const toggleTheme = () => {
+	theme.global.name.value = theme.global.current.value.dark
+		? 'myTheme'
+		: 'dark';
+	mode = theme.global.name.value;
+};
+</script>
